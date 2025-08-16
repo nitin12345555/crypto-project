1271a184e4815139356ca6e878eaa061d8de137f
Nitin Jangid <nitinjangidj92@gmail.com>
2025-08-16 16:06:11 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,8 @@
-
 import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-       
-        await mongoose.connect(`${process.env.MONGODB_URL}/crypto-user`);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log("Database Connected");
     } catch (error) {
         console.error("Database connection error:", error.message);
