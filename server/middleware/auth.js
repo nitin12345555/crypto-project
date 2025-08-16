7cfe88b1a56f83cdabfc97969d560478a5a919f7
Nitin Jangid <nitinjangidj92@gmail.com>
2025-08-16 16:14:35 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,32 +1,30 @@
-
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
 export const protect = async (req, res, next) => {
-  let token;
+    let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  } else {
-    
-    token = req.headers.authorization;
-  }
-
-  if (!token) {
-    return res.json({ success: false, message: "Not authorized, no token" });
-  }
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); 
-    req.user = await User.findById(decoded.id).select("-password");
-    if (!req.user) {
-      return res.json({ success: false, message: "User not found" });
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
+    } else {
+        token = req.headers.authorization;
     }
-    next();
-  } catch (error) {
-    return res.json({ success: false, message: "Not authorized, token invalid" });
-  }
+
+    if (!token) {
+        return res.json({ success: false, message: "Not authorized, no token" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        if (!req.user) {
+            return res.json({ success: false, message: "User not found" });
+        }
+        next();
+    } catch (error) {
+        return res.json({ success: false, message: "Not authorized, token invalid" });
+    }
 };
